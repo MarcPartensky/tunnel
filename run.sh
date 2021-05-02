fd4e4c4261c66b0c2cbd439fb0f2e12cfc4d910b
Marc Partensky <marc.partensky@gmail.com>
2021-05-02 18:36:40 +0200

    most solid ssh conf there is

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+export AUTOSSH_LOGFILE="./mini.log"
+
 # $port=7122
+
+logs() {
+	tail -f $AUTOSSH_LOGFILE
+}
 
 iautossh() {
 	tar xvf autossh-1.4g.tgz
@@ -13,19 +19,6 @@ iautossh() {
 }
 
 tunnel() {
-	ssh -f -N -T \
-		-L 7122:marcpartensky.com:7122 \
-		root@marcpartensky.com
-}
-
-rtunnel() {
-	ssh -f -N -T \
-		-R 7122:localhost:7122 \
-		root@marcpartensky.com
-}
-
-
-atunnel() {
 	autossh -f -M 7123 -N \
 		-o "PubkeyAuthentication=yes" \
 		-o "PasswordAuthentication=no" \
@@ -33,10 +26,12 @@ atunnel() {
 		-o "ServerAliveCountMax 3" \
 		-o ExitOnForwardFailure=yes \
 		-L 7122:localhost:7122 \
-		root@marcpartensky.com -p 7022
+		-p 7022 \
+		-i ~/.ssh/tunnel \
+		tunnel@marcpartensky.com
 }
 
-artunnel() {
+rtunnel() {
 	autossh -f -M 7123 -N \
 		-o "PubkeyAuthentication=yes" \
 		-o "PasswordAuthentication=no" \
@@ -44,7 +39,9 @@ artunnel() {
 		-o "ServerAliveCountMax 3" \
 		-o ExitOnForwardFailure=yes \
 		-R 7122:localhost:22 \
-			root@marcpartensky.com -p 7022
+		-p 7022 \
+		-i ~/.ssh/tunnel \
+		tunnel@marcpartensky.com
 }
 
 control() {
