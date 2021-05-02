d356972ff7250728f915b5b16b915a35a05da6b3
Marc Partensky <marc@Netinsoft.fr>
2021-05-02 13:29:06 +0200

    simple installation

--- run.sh
+++ run.sh
@@ -2,11 +2,21 @@
 
 # $port=7122
 
-function tunnel {
+iautossh() {
+	tar xvf autossh-1.4g.tgz
+	cd autossh-1.4g
+	./configure
+	make
+	chmod +x autossh
+	mv /autossh /usr/local/bin
+	cd -
+}
+
+tunnel() {
 	ssh -f -N -T 7122:marcpartensky.com:7122 root@marcpartensky.com
 }
 
-function rtunnel {
+rtunnel() {
 	ssh -f -N -T -R7122:localhost:7122 root@marcpartensky.com
 }
 
@@ -19,7 +29,7 @@ artunnel() {
 	autossh -f -M 0 -N -o "ServerAliveInterval 10" -o "ServerAliveCountMax 3" -o ExitOnForwardFailure=yes -R 7122:localhost:7122 root@marcpartensky.com
 }
 
-function control {
+control() {
 	sudo docker run -it \
 		-v /:/system \
 		-p 7122:7122 \
