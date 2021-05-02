b1f33c6db28cfd94d5814f8f344b1a46f8973944
marc <marc.partensky@gmail.com>
2021-05-02 16:36:09 +0200

    le changement c'est maintenant

--- run.sh
+++ run.sh
@@ -13,20 +13,38 @@ iautossh() {
 }
 
 tunnel() {
-	ssh -f -N -T 7122:marcpartensky.com:7122 root@marcpartensky.com
+	ssh -f -N -T \
+		-L 7122:marcpartensky.com:7122 \
+		root@marcpartensky.com
 }
 
 rtunnel() {
-	ssh -f -N -T -R7122:localhost:7122 root@marcpartensky.com
+	ssh -f -N -T \
+		-R 7122:localhost:7122 \
+		root@marcpartensky.com
 }
 
 
 atunnel() {
-	autossh -f -M 0 -N -o "ServerAliveInterval 10" -o "ServerAliveCountMax 3" -o ExitOnForwardFailure=yes -L 7122:localhost:7122 root@marcpartensky.com -p 7022
+	autossh -f -M 7123 -N \
+		-o "PubkeyAuthentication=yes" \
+		-o "PasswordAuthentication=no" \
+		-o "ServerAliveInterval 10" \
+		-o "ServerAliveCountMax 3" \
+		-o ExitOnForwardFailure=yes \
+		-L 7122:localhost:7122 \
+		root@marcpartensky.com -p 7022
 }
 
 artunnel() {
-	autossh -f -M 7123 -N -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -o "ServerAliveInterval 10" -o "ServerAliveCountMax 3" -o ExitOnForwardFailure=yes -R 7122:localhost:22 root@marcpartensky.com -p 7022
+	autossh -f -M 7123 -N \
+		-o "PubkeyAuthentication=yes" \
+		-o "PasswordAuthentication=no" \
+		-o "ServerAliveInterval 10" \
+		-o "ServerAliveCountMax 3" \
+		-o ExitOnForwardFailure=yes \
+		-R 7122:localhost:22 \
+			root@marcpartensky.com -p 7022
 }
 
 control() {
