8f4dfd0032872eb96fdddf21b2c317aeabd7fa17
marc <marc.partensky@gmail.com>
2021-11-06 21:11:57 +0100

    good setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
 if [ $1 = "install" ]; then
-	for service in $(/bin/ls service/*service); do
-		ln -sf $PWD/service/$service /etc/systemd/system/$service
+	cd service
+	for service in $(/bin/ls *service); do
+		echo $service
+		ln -sf $PWD/$service /etc/systemd/system/$service
 	done
 	systemctl daemon-reload
+	cd -
 fi
 
 if [ $1 = "start" ]; then
 	if [ $# = 1]; then
+		cd service
 		for service in $(/bin/ls service/*service); do
 			systemctl start $service
 			systemctl enable $service
 		done
+		cd -
 	else
 		systemctl start $2
 		systemctl enable $2
