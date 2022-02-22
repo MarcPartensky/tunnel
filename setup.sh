e160473343d5a28c2b4c7719d6e64d1147415728
Marc Partensky <marc.partensky@gmail.com>
2022-02-22 12:39:02 +0100

    user services from now on

--- setup.sh
+++ setup.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
-if [ $1 = "install" ]; then
-	cd service
-	for service in $(/bin/ls *service); do
-		echo $service
-		ln -sf $PWD/$service /etc/systemd/system/$service
-	done
-	systemctl daemon-reload
-	cd -
-fi
+ln -sf $PWD/service $HOME/.config/systemd/user
 
-if [ $1 = "start" ]; then
-	if [ $# = 1]; then
-		cd service
-		for service in $(/bin/ls service/*service); do
-			systemctl start $service
-			systemctl enable $service
-		done
-		cd -
-	else
-		systemctl start $2
-		systemctl enable $2
-	fi
-fi
+# if [ $1 = "install" ]; then
+# 	cd service
+# 	for service in $(/bin/ls *service); do
+# 		echo $service
+# 		ln -sf $PWD/$service /etc/systemd/system/$service
+# 	done
+# 	systemctl daemon-reload
+# 	cd -
+# fi
+
+# if [ $1 = "start" ]; then
+# 	if [ $# = 1]; then
+# 		cd service
+# 		for service in $(/bin/ls service/*service); do
+# 			systemctl start $service
+# 			systemctl enable $service
+# 		done
+# 		cd -
+# 	else
+# 		systemctl start $2
+# 		systemctl enable $2
+# 	fi
+# fi
